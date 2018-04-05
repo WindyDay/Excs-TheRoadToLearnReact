796ff37ee5952b8c8d6c641702b91aeb7fd0a9c5
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2018-04-05 21:56:22 +0700

    eact create-react-app bootstraps a React application JSX mixes up HTML and JavaScript to define the output of React components in their render methods components, instances and elements are different things in React ReactDOM.render() is an entry point for a React application to hook React into the DOM built-in JavaScript functionalities can be used in JSX map can be used to render a list of items as HTML elements ES6 variable declarations with const and let can be used for specific use cases use const over let in React applications arrow functions can be used to keep your functions concise classes are used to define components in React by extending them

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 class App extends Component {
   render() {
+    let helloWord = "Welcome to the Road to learn react";
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{helloWord}</h1>
+        {list.map(item=>
+            <div key={item.objectID}>
+              <a href={item.url}>{item.title} - {item.author} - {item.points} point - {item.num_comments} cmt</a>
+            </div>
+        )}
       </div>
     );
   }
