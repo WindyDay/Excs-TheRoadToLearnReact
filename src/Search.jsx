62199834ddeb75b3e1b6af56ec2892ebe56ca7c2
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2018-04-14 02:36:33 +0700

    Auto focus on search bar

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Button from './Button'
 
-export function Search({searchTerm, className, onSeachChange, onSubmit, children}) {
-    return (
-        <form onSubmit={onSubmit}>
-            <div className='row'>
-                <div className='col-md-7 offset-md-2 '>
-                    <input  type="search" 
-                        className={className}
-                        value={searchTerm}
-                        onChange={onSeachChange}
-                        placeholder={children.text}
-                    />
+export class Search extends Component {
+    componentDidMount(){
+        if(this.input){
+            this.input.focus();
+        }
+    }
+
+    render() {
+        const {searchTerm, className, onSeachChange, onSubmit, children} = this.props;
+        return (
+            <form onSubmit={onSubmit}>
+                <div className='row'>
+                    <div className='col-md-7 offset-md-2 '>
+                        <input
+                            type="search"
+                            className={className}
+                            value={searchTerm}
+                            onChange={onSeachChange}
+                            placeholder={children.text}
+                            ref = {(node) =>{ this.input = node}}
+                            />
+                    </div>
+                    <div className='col-md-1'>
+                        <Button type='submit' className='btn btn-success'>Search</Button>
+                    </div>
                 </div>
-                <div className='col-md-1'>
-                    <Button type='submit' className='btn btn-success'>Search</Button>
-                </div>
-            </div>  
-      </form>
-    )
+            </form>
+        )
+    }
+
 }
 
 export default Search
